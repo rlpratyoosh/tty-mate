7b7193117443f7316b5445897d359fa7fbd9d67c
rlpratyoosh <pratyoosh.prakash.dev@gmail.com>
2026-05-09 19:53:39 +0530

    feat: add get possible moves function & get possible moves for pawn

--- src/lib.rs
+++ src/lib.rs
@@ -100,6 +100,51 @@ impl Board {
         (row, col)
     }
 
+    pub fn get_possible_moves(&self, idx: usize) -> Result<MoveList, &str> {
+        let Some(piece) = self.square[idx] else {
+            return Err("No piece at the given index");
+        };
+        match piece.piece_type {
+            PieceType::Pawn => Ok(self.get_possible_pawn_moves(piece, idx)),
+            _ => unimplemented!(),
+        }
+    }
+
+    fn get_possible_pawn_moves(&self, piece: Piece, idx: usize) -> MoveList {
+        let mut result = MoveList::new();
+        let (row, col) = Board::index_to_coordinates(idx);
+        let r = row as isize;
+        let c = col as isize;
+        let row_step: isize = if piece.piece_color == PieceColor::White { 1 } else { -1 };
+
+        // Check forward
+        for j in [1, 2] {
+            let row_step = j*row_step;
+            if (r + row_step) > 7 || (r + row_step) < 0 {
+                break;
+            }
+            let target_idx = 8*((r+row_step) as usize)+col;
+            let None = self.square[target_idx] else { break; };
+            if j==2 && !((row == 1 && piece.piece_color == PieceColor::White) || (row == 6 && piece.piece_color == PieceColor::Black)) {
+                break;
+            }
+            result.push(target_idx)
+        }
+
+        // Check Diagonally
+        for col_step in [1, -1] {
+            if (r + row_step) >= 0 && (r + row_step) <= 7 && (c + col_step) >= 0 && (c + col_step) <= 7 {
+                let target_idx = (8 * (r+row_step) + (c + col_step)) as usize;
+                if let Some(target_piece) = self.square[target_idx] {
+                    if target_piece.piece_color != piece.piece_color {
+                        result.push(target_idx);
+                    }
+                }
+            }
+        }
+
+        result
+    }
 }
 
 impl fmt::Display for Board {
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,5 @@ use tty_mate::Board;
 fn main() {
     let board = Board::new();
     println!("{}", board);
+    println!("{:?}", board.get_possible_moves(49));
 }
